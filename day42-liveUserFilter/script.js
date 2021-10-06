e0876e98921b46f08ed834bb8df4958083075208
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2021-10-05 22:52:01 -0300

    day forty two

---
+++ day42-liveUserFilter/script.js
@@ -0,0 +1,42 @@
+const input = document.getElementById('filter')
+const result = document.getElementById('result')
+const listItems = []
+
+getData()
+
+input.addEventListener('input', (e) => filterData(e.target.value))
+
+async function getData() {
+    const response = await fetch('https://randomuser.me/api?results=50')
+
+    const { results } = await response.json()
+    // console.log(results)
+    
+    result.innerHTML = ''
+
+    results.forEach(user => {
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}" />
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
+
+        result.appendChild(li)
+    })
+}
+
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if (item.innerHTML.toUpperCase().includes(searchTerm.toUpperCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
+    })
+}
